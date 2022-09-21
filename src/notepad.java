734593b67336dca47ce17794fefd045cb8d39f81
wyj-proj <1078075534@qq.com>
2022-09-21 16:43:18 +0800

    Add a comment "Main function"

--- src/notepad.java
+++ src/notepad.java
@@ -10,6 +10,7 @@ import java.util.Date;
 
 public class notepad extends JFrame{
 
+    //Main function
     public static void main(String[] args) {
         notepad notepad = new notepad();
         notepad.setDefaultCloseOperation(EXIT_ON_CLOSE);
