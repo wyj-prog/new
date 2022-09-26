5eebbfd0c799a2e2e0993218b15d8ef799e8f37c
postludeR <nemopostlude@gmail.com>
2022-09-26 20:22:30 +0800

    little thing

--- src/notepad.java
+++ src/notepad.java
@@ -12,7 +12,6 @@ import java.util.Date;
 public class notepad extends JFrame{
 
     public static void main(String[] args) {
-        System.out.println("adad");
         notepad notepad = new notepad();
         notepad.setDefaultCloseOperation(EXIT_ON_CLOSE);
 //        new notepad();
