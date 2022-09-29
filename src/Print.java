bae001fd30179eddbd60972a474df64405a9d2a5
wyjgit <1078075534@qq.com>
2022-09-29 21:22:20 +0800

    Fixed the bug of print function

--- src/Print.java
+++ src/Print.java
@@ -9,23 +9,6 @@ import java.awt.print.*;
 
 public class Print extends JFrame {
     public Print(){
-//        //Initializing the window
-//        this.setTitle("Print");
-//        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        JPanel jPanel = new JPanel();
-//        jPanel.setLayout(null);
-//        JButton Print_Button = new JButton("Print");
-//        JButton Cancel_Button = new JButton("Cancel");
-//        JButton Apply_Button = new JButton("Apply");
-//        Print_Button.setBounds(new Rectangle(100, 320, 80, 20));
-//        Cancel_Button.setBounds(new Rectangle(200,320,80,20));
-//        Apply_Button.setBounds(new Rectangle(300,320,80,20));
-//        jPanel.add(Print_Button);
-//        jPanel.add(Cancel_Button);
-//        jPanel.add(Apply_Button);
-//        this.add(jPanel);
-//        this.setBounds(funtions.getScreenWidth()/2 - 210, funtions.getScreenHeight()/2 - 190, 420, 380);
-//        this.setVisible(true);
         //Invoke the print dialog to print the document to the user
             try{
                 //Build a print property set
@@ -44,7 +27,7 @@ public class Print extends JFrame {
                     DocPrintJob job = service.createPrintJob();
                     DocAttributeSet das = new HashDocAttributeSet();
                     //Create a print file format
-                    Doc doc = new SimpleDoc(notepad.leftPart.getText().getBytes(), flavor, das);
+                    Doc doc = new SimpleDoc(notepad.input.getText().getBytes(), flavor, das);
                     //print file
                     job.print(doc, pras);
                 }
