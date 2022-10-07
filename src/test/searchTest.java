a1fefbaea50e5e5215852a73f911f58b5cf27c2b
postludeR <nemopostlude@gmail.com>
2022-10-07 22:35:25 +0800

    bigChange

--- src/test/searchTest.java
+++ src/test/searchTest.java
@@ -5,5 +5,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class searchTest {
     @Test
     void searchMethod() {
+        notepad notepadT = new notepad();
+        search searchT = new search();
+
+        notepadT.input.setText("Beneath this mask there is more than flesh." +
+                "Beneath this mask there is an idea, " +
+                "Mr. Creedy, and ideas are bulletproof.");
+        searchT.inputS.setText("is");
+
+        assertEquals(10, search.searchMethod());
+        assertEquals(24, search.searchMethod());
+        assertEquals(53, search.searchMethod());
+        assertEquals(67, search.searchMethod());
+        assertEquals(10, search.searchMethod());
     }
 }
